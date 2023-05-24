073060f02af45bdf70d95f02124732c09cb65270
David Okpako <105345335+timeax@users.noreply.github.com>
2023-05-24 19:30:31 +0100

    update

--- dist/test/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const modules_1 = require("../modules");
-modules_1.Fs.createDirs(modules_1.Fs.join(__dirname, '../../names/'));
